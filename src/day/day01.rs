e1e7d0a1260525b6ef5c428c6bf22c8a07e27406
Jacques Schutte <jqschutte@gmail.com>
2023-12-01 20:56:34 +0200

    Day 1 - revised

--- src/day/day01.rs
+++ src/day/day01.rs
@@ -2,55 +2,95 @@ use super::Day;
 use crate::input::Input;
 
 #[derive(Debug)]
-struct Line {
-    calibration: u32,
-    calibration2: u32,
+struct Line(Vec<u8>);
+
+impl From<&str> for Line {
+    fn from(value: &str) -> Self {
+        Self(value.bytes().collect())
+    }
 }
 
-impl From<String> for Line {
-    fn from(mut value: String) -> Self {
-        let mut calibration = 0;
-        for c in value.chars() {
-            if let Some(digit) = c.to_digit(10) {
-                calibration += digit * 10;
-                break;
-            }
-        }
-        for c in value.chars().rev() {
-            if let Some(digit) = c.to_digit(10) {
-                calibration += digit;
-                break;
+impl Line {
+    fn digits(&self) -> DigitIter {
+        DigitIter { values: &self.0 }
+    }
+
+    fn digits_and_words(&self) -> DigitAndWordIter {
+        DigitAndWordIter { values: &self.0 }
+    }
+
+    fn calibration(&self) -> u8 {
+        let tens = self.digits().next().unwrap();
+        let ones = self.digits().last().unwrap();
+
+        tens * 10 + ones
+    }
+
+    fn calibration2(&self) -> u8 {
+        let tens = self.digits_and_words().next().unwrap();
+        let ones = self.digits_and_words().last().unwrap();
+
+        tens * 10 + ones
+    }
+}
+
+struct DigitIter<'a> {
+    values: &'a [u8],
+}
+
+impl Iterator for DigitIter<'_> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some((c, new_values)) = self.values.split_first() {
+            self.values = new_values;
+            if let Some(d) = c.is_ascii_digit().then_some(c - b'0') {
+                return Some(d);
             }
         }
 
-        value = value.replace("one", "one1one");
-        value = value.replace("two", "two2two");
-        value = value.replace("three", "three3three");
-        value = value.replace("four", "four4four");
-        value = value.replace("five", "five5five");
-        value = value.replace("six", "six6six");
-        value = value.replace("seven", "seven7seven");
-        value = value.replace("eight", "eight8eight");
-        value = value.replace("nine", "nine9nine");
+        None
+    }
+}
 
-        let mut calibration2 = 0;
-        for c in value.chars() {
-            if let Some(digit) = c.to_digit(10) {
-                calibration2 += digit * 10;
-                break;
+struct DigitAndWordIter<'a> {
+    values: &'a [u8],
+}
+
+impl Iterator for DigitAndWordIter<'_> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while !self.values.is_empty() {
+            const CONVERT: [(&[u8], u8); 9] = [
+                (b"one", 1),
+                (b"two", 2),
+                (b"three", 3),
+                (b"four", 4),
+                (b"five", 5),
+                (b"six", 6),
+                (b"seven", 7),
+                (b"eight", 8),
+                (b"nine", 9),
+            ];
+
+            let digit = CONVERT
+                .iter()
+                .find_map(|(word, digit)| self.values.starts_with(word).then_some(*digit))
+                .or_else(|| {
+                    self.values
+                        .first()
+                        .and_then(|c| c.is_ascii_digit().then_some(c - b'0'))
+                });
+
+            self.values = &self.values[1..];
+
+            if digit.is_some() {
+                return digit;
             }
         }
-        for c in value.chars().rev() {
-            if let Some(digit) = c.to_digit(10) {
-                calibration2 += digit;
-                break;
-            }
-        }
 
-        Self {
-            calibration,
-            calibration2,
-        }
+        None
     }
 }
 
@@ -61,24 +101,26 @@ pub(crate) struct Day01 {
 
 impl Day for Day01 {
     fn setup(&mut self) -> color_eyre::eyre::Result<()> {
-        self.values = Input::get(1)?
-            .data
-            .lines()
-            .map(|s| s.to_string().into())
-            .collect();
+        self.values = Input::get(1)?.data.lines().map(|s| s.into()).collect();
 
         Ok(())
     }
 
     fn part1(&self) -> color_eyre::eyre::Result<String> {
-        let total: u32 = self.values.iter().map(|v| v.calibration).sum();
-
-        Ok(total.to_string())
+        Ok(self
+            .values
+            .iter()
+            .map(|v| v.calibration() as usize)
+            .sum::<usize>()
+            .to_string())
     }
 
     fn part2(&self) -> color_eyre::eyre::Result<String> {
-        let total: u32 = self.values.iter().map(|v| v.calibration2).sum();
-
-        Ok(total.to_string())
+        Ok(self
+            .values
+            .iter()
+            .map(|v| v.calibration2() as usize)
+            .sum::<usize>()
+            .to_string())
     }
 }
